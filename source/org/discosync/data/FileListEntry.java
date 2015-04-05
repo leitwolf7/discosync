6bb78aceac1a1b003a70724e36bf7ba04824bcf9
leitwolf7 <leitwolf@wolke7.net>
2015-04-05 19:28:23 +0200

    Add copyright headers.

--- source/org/discosync/data/FileListEntry.java
+++ source/org/discosync/data/FileListEntry.java
@@ -1,9 +1,27 @@
+/*
+ * This file is part of DiscoSync (home: github.com, leitwolf7/discosync)
+ * 
+ * Copyright (C) 2015, 2015 leitwolf7
+ *
+ *  DiscoSync is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DiscoSync is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DiscoSync.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.discosync.data;
 
 /*
  * Created on 01.04.2015
  */
-
 public class FileListEntry {
     protected String path;
     protected long checksum; 
